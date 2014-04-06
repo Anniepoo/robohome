263e9ddaee80c1b544e88b54ff33c79c5517a820
Anniepoo <annie66us@yahoo.com>
2014-04-05 21:42:55 -0700

    Intial checkin of wpf version of file saving kinectstream

---
+++ interfaces/kinectstreamwpf/MainWindow.xaml.cs
@@ -0,0 +1,354 @@
+﻿//------------------------------------------------------------------------------
+// <copyright file="MainWindow.xaml.cs" company="Microsoft">
+//     Copyright (c) Microsoft Corporation.  All rights reserved.
+// </copyright>
+//------------------------------------------------------------------------------
+
+namespace Microsoft.Samples.Kinect.DepthBasics
+{
+    using System;
+    using System.Globalization;
+    using System.IO;
+    using System.Windows;
+    using System.Windows.Media;
+    using System.Windows.Media.Imaging;
+    using Microsoft.Kinect;
+    using System.Text;
+    using System.Net.Sockets;
+    using System.Net;
+
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        /// <summary>
+        /// Active Kinect sensor
+        /// </summary>
+        private KinectSensor sensor;
+
+        /// <summary>
+        /// Bitmap that will hold color information
+        /// </summary>
+        private WriteableBitmap colorBitmap;
+
+        /// <summary>
+        /// Intermediate storage for the depth data received from the camera
+        /// </summary>
+        private DepthImagePixel[] depthPixels;
+
+        /// <summary>
+        /// Intermediate storage for the depth data converted to color
+        /// </summary>
+        private byte[] colorPixels;
+
+        /// <summary>
+        /// time between images written to disk
+        /// </summary>
+        private float freq = 1.0f;
+
+        private string imageDir = Environment.CurrentDirectory;
+
+        private long nextSaveTick = 0L;
+
+        /// <summary>
+        /// Initializes a new instance of the MainWindow class.
+        /// </summary>
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        const float MINFREQ = 0.05f;
+
+        /// <summary>
+        /// Execute startup tasks
+        /// </summary>
+        /// <param name="sender">object sending the event</param>
+        /// <param name="e">event arguments</param>
+        private void WindowLoaded(object sender, RoutedEventArgs e)
+        {
+            // Look through all sensors and start the first connected one.
+            // This requires that a Kinect is connected at the time of app startup.
+            // To make your app robust against plug/unplug, 
+            // it is recommended to use KinectSensorChooser provided in Microsoft.Kinect.Toolkit
+            foreach (var potentialSensor in KinectSensor.KinectSensors)
+            {
+                if (potentialSensor.Status == KinectStatus.Connected)
+                {
+                    this.sensor = potentialSensor;
+                    break;
+                }
+            }
+            
+            if (null != this.sensor)
+            {
+                // Turn on the depth stream to receive depth frames
+                this.sensor.DepthStream.Enable(DepthImageFormat.Resolution320x240Fps30);   // DepthImageFormat.Resolution640x480Fps30);
+                
+                // Allocate space to put the depth pixels we'll receive
+                this.depthPixels = new DepthImagePixel[this.sensor.DepthStream.FramePixelDataLength];
+
+                // Allocate space to put the color pixels we'll create
+                this.colorPixels = new byte[this.sensor.DepthStream.FramePixelDataLength * sizeof(int)];
+
+                // This is the bitmap we'll display on-screen
+                this.colorBitmap = new WriteableBitmap(this.sensor.DepthStream.FrameWidth, this.sensor.DepthStream.FrameHeight, 96.0, 96.0, PixelFormats.Bgr32, null);
+
+                // Set the image we display to point to the bitmap where we'll put the image data
+                this.Image.Source = this.colorBitmap;
+
+                // Add an event handler to be called whenever there is new depth frame data
+                this.sensor.DepthFrameReady += this.SensorDepthFrameReady;
+
+                // Start the sensor!
+                try
+                {
+                    this.sensor.Start();
+                }
+                catch (IOException)
+                {
+                    this.sensor = null;
+                }
+            }
+
+            if (null == this.sensor)
+            {
+                this.statusBarText.Text = Properties.Resources.NoKinectReady;
+            }
+
+            // lets set up to save images
+            string[] args = Environment.GetCommandLineArgs();
+
+            for (int i = 0; i < args.Length - 1; i++)
+            {
+                if (args[i] == "-freq")
+                {
+                    freq = float.Parse(args[++i]);
+                    if (freq < MINFREQ) freq = 1.0f;
+                }
+                else if (args[i] == "-img")
+                {
+                    imageDir = args[++i];
+                }
+            }
+        }
+
+        /// <summary>
+        /// Execute shutdown tasks
+        /// </summary>
+        /// <param name="sender">object sending the event</param>
+        /// <param name="e">event arguments</param>
+        private void WindowClosing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            if (null != this.sensor)
+            {
+                this.sensor.Stop();
+            }
+        }
+
+        /// <summary>
+        /// Write the most recent depth image out the socket as raw shorts
+        /// </summary>
+        /// <param name="bw"></param>
+        public void writeDepthRaw(Socket s)
+        {
+            try
+            {
+                lock (this.depthPixels)  // TODO deal with no image avail
+                {
+                    byte[] stupidtypes = new byte[this.depthPixels.Length * 2];
+                    int j = 0;
+                    for (int i = 0; i < this.depthPixels.Length; ++i)
+                    {
+                        // Get the depth for this pixel
+                        short depth = depthPixels[i].Depth;
+                        
+                        stupidtypes[j++] = (byte)(depth >> 8);
+                        stupidtypes[j++] = (byte)(depth % 256);  // sign isn't important because we're only getting 12 bits
+                    }
+
+                    s.Send(stupidtypes);
+                }
+            }
+            catch (Exception)
+            {
+                
+                throw;
+            }
+        }
+
+        /// <summary>
+        /// Event handler for Kinect sensor's DepthFrameReady event
+        /// </summary>
+        /// <param name="sender">object sending the event</param>
+        /// <param name="e">event arguments</param>
+        private void SensorDepthFrameReady(object sender, DepthImageFrameReadyEventArgs e)
+        {
+            using (DepthImageFrame depthFrame = e.OpenDepthImageFrame())
+            {
+                if (depthFrame != null)
+                {
+                    if (DateTime.UtcNow.Ticks < nextSaveTick)
+                        return;
+
+                    nextSaveTick = DateTime.UtcNow.AddSeconds(freq).Ticks; 
+
+                    // Copy the pixel data from the image to a temporary array
+                    lock (this.depthPixels)
+                    {
+                        depthFrame.CopyDepthImagePixelDataTo(this.depthPixels);
+
+                        // Get the min and max reliable depth for the current frame
+                        int minDepth = depthFrame.MinDepth;
+                        int maxDepth = depthFrame.MaxDepth;
+
+                        // Convert the depth to RGB
+                        int colorPixelIndex = 0;
+                        for (int i = 0; i < this.depthPixels.Length; ++i)
+                        {
+                            // Get the depth for this pixel
+                            short depth = depthPixels[i].Depth;
+
+                            // To convert to a byte, we're discarding the most-significant
+                            // rather than least-significant bits.
+                            // We're preserving detail, although the intensity will "wrap."
+                            // Values outside the reliable depth range are mapped to 0 (black).
+
+                            // Note: Using conditionals in this loop could degrade performance.
+                            // Consider using a lookup table instead when writing production code.
+                            // See the KinectDepthViewer class used by the KinectExplorer sample
+                            // for a lookup table example.
+                            byte intensity = (byte)(depth >= minDepth && depth <= maxDepth ? depth : 0);
+
+                            if (!depthPixels[i].IsKnownDepth)
+                            {
+                                this.colorPixels[colorPixelIndex++] = 0xFF;      // b
+                                this.colorPixels[colorPixelIndex++] = 0xFF;      // g
+                                this.colorPixels[colorPixelIndex++] = 0xFF;   // r
+                            }
+                            else if (depth < minDepth)
+                            {
+                                this.colorPixels[colorPixelIndex++] = 0xFF;      // b
+                                this.colorPixels[colorPixelIndex++] = 0;      // g
+                                this.colorPixels[colorPixelIndex++] = 0xFF;   // r
+                            }
+                            else if (depth > maxDepth)
+                            {
+                                this.colorPixels[colorPixelIndex++] = 0xFF;
+                                this.colorPixels[colorPixelIndex++] = 0;
+                                this.colorPixels[colorPixelIndex++] = 0;
+                            }
+                            else
+                            {
+                                // Write out blue byte
+                                this.colorPixels[colorPixelIndex++] = (byte)(depthPixels[i].PlayerIndex * 16); // (byte)depth;
+
+                                // Write out green byte
+                                this.colorPixels[colorPixelIndex++] = (byte)((depth % 16) << 4);
+
+                                // Write out red byte    
+                                this.colorPixels[colorPixelIndex++] = (byte)(0xFF - (byte)(depth / 16));
+                            }
+
+                            // We're outputting BGR, the last byte in the 32 bits is unused so skip it
+                            // If we were outputting BGRA, we would write alpha here.
+                            ++colorPixelIndex;
+                        }
+                    }
+
+                    // Write the pixel data into our bitmap
+                    this.colorBitmap.WritePixels(
+                        new Int32Rect(0, 0, this.colorBitmap.PixelWidth, this.colorBitmap.PixelHeight),
+                        this.colorPixels,
+                        this.colorBitmap.PixelWidth * sizeof(int),
+                        0);
+
+                    // persist to file
+                    persistToFile();
+                }
+            }
+        }
+
+        /// <summary>
+        /// Handles the user clicking on the screenshot button
+        /// </summary>
+        /// <param name="sender">object sending the event</param>
+        /// <param name="e">event arguments</param>
+        private void ButtonScreenshotClick(object sender, RoutedEventArgs e)
+        {
+            persistToFile();
+        }
+
+        const int RINGBUFFERLENGTH = 10;
+
+        private int ringBufferCursor = 0;
+
+        private void persistToFile()
+        {
+            if (null == this.sensor)
+            {
+                this.statusBarText.Text = Properties.Resources.ConnectDeviceFirst;
+                return;
+            }
+
+            // create a png bitmap encoder which knows how to save a .png file
+            BitmapEncoder encoder = new PngBitmapEncoder();
+
+            // create frame from the writable bitmap and add to encoder
+            encoder.Frames.Add(BitmapFrame.Create(this.colorBitmap));
+
+            string time = System.DateTime.Now.ToString("hh'-'mm'-'ss", CultureInfo.CurrentUICulture.DateTimeFormat);
+            
+           // string myPhotos = Environment.GetFolderPath(Environment.SpecialFolder.MyPictures);
+
+            string path = Path.Combine(imageDir, "KinectSnapshot-" + ringBufferCursor + ".png");
+            ringBufferCursor += 1;
+            if (ringBufferCursor >= RINGBUFFERLENGTH)
+                ringBufferCursor = 0;
+            Console.WriteLine(path);
+
+            // write the new file to disk
+            try
+            {
+                using (FileStream fs = new FileStream(path, FileMode.Create))
+                {
+                    encoder.Save(fs);
+                }
+
+                this.statusBarText.Text = string.Format("{0} {1}", Properties.Resources.ScreenshotWriteSuccess, path);
+            }
+            catch (IOException)
+            {
+                this.statusBarText.Text = string.Format("{0} {1}", Properties.Resources.ScreenshotWriteFailed, path);
+            }
+        }
+        
+        /// <summary>
+        /// Handles the checking or unchecking of the near mode combo box
+        /// </summary>
+        /// <param name="sender">object sending the event</param>
+        /// <param name="e">event arguments</param>
+        private void CheckBoxNearModeChanged(object sender, RoutedEventArgs e)
+        {
+            if (this.sensor != null)
+            {
+                // will not function on non-Kinect for Windows devices
+                try
+                {
+                    if (this.checkBoxNearMode.IsChecked.GetValueOrDefault())
+                    {
+                        this.sensor.DepthStream.Range = DepthRange.Near;
+                    }
+                    else
+                    {
+                        this.sensor.DepthStream.Range = DepthRange.Default;
+                    }
+                }
+                catch (InvalidOperationException)
+                {
+                }
+            }
+        }
+    }
+}
